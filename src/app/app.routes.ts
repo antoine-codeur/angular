f67eb0824f561d14bf3fc2c56875a4534ec8f2ea
antoine-codeur <antoinejf.denoue@gmail.com>
2024-05-11 11:37:45 +0200

    creation d'une app aguluar avec navigation entre les pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
 
-export const routes: Routes = [];
+import { LoginComponent } from './views/auth/login/login.component';
+import { RegisterComponent } from './views/auth/register/register.component';
+
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
